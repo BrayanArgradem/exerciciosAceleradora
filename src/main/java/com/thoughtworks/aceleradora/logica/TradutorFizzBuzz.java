2f83daeb366fb8803eac534dec7b4c7df1351f80
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-08-05 16:08:31 -0300

    exercicios fizzbuzz

--- src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
@@ -3,6 +3,21 @@ package com.thoughtworks.aceleradora.logica;
 public class TradutorFizzBuzz {
 
     String traduz(int numero) {
+        if (numero % 3 == 0 && numero % 5 == 0 ){
+        return "FizzBuzz";
+    }
+        if (numero % 3 == 0) {
+            return "Fizz";
+        } else {
+            System.out.println("nada");
+        }
+//        String numero = Integer.parseInt(3); //numero == "2"
+
+        if (numero % 5 == 0){
+            return "Buzz";
+        }
+
         return null;
     }
+
 }
